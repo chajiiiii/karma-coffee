ca7b0fe6c1d3fbb23aa2342585c00c8b84411b1e
chajiiiii <j.cha1708@gmail.com>
2023-08-10 12:55:51 +0100

    Update Stripe

--- main_app/views.py
+++ main_app/views.py
@@ -20,7 +20,7 @@ from .forms import ReviewForm
 
 stripe.api_key = settings.STRIPE_API_KEY_HIDDEN
 
-YOUR_DOMAIN = 'http://localhost:8000'
+YOUR_DOMAIN = 'https://karma-coffee-abe41bfb39f9.herokuapp.com'
 
 # Create your views here.
 
